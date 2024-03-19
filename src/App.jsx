5b412e5af0a5c633bf102d921ec030674b96a1d0
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-19 13:03:04 +0200

    step-2 has completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,36 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [total, setTotal] = useState(0)
+  
+
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+    
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
+    
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
+    setTotal(updatedBad + good + neutral)
+    
   }
+
+  // const Statistics = ({good, neutral, bad}) => {
+
+  // }
+  const all = good + neutral + bad
+  const averageScore = (good * 1 + neutral * 0 + bad * (-1)) / all
+  const positivePercentage = (good / all) * 100
 
   return (
     <div>
@@ -33,6 +49,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {averageScore}</p>
+      <p>positive {positivePercentage}%</p>
 
 
     </div>
